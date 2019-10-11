9c29e98ab0ea5382ce9be6fed07560ca59b42ac5
qizhu8 <qizhu8@126.com>
2019-10-11 00:53:55 -0400

    add scrolledtext, buttons

--- src/DocManagerGUI.py
+++ src/DocManagerGUI.py
@@ -4,10 +4,10 @@
 import os
 import json
 import tkinter as tk
+import tkinter.scrolledtext as tkst
 from DocManager import DocManager
 
 VERSION = "1.0.0"
-WIDTH, HEIGHT = 200, 300
 
 class DocManagerGUI(tk.Tk, object):
     """
@@ -124,11 +124,20 @@ class DocManagerGUI(tk.Tk, object):
         self.document_list_ctl_dict['list'].bind("<<ListboxSelect>>", __list_select)
         self.document_list_ctl_dict['list'].pack(fill=tk.BOTH, expand=1)
 
+        self.document_list_ctl_dict['scrollbar'] = tk.Scrollbar(self.document_list_frame, orient="vertical")
+        self.document_list_ctl_dict['scrollbar'].config(command=self.document_list_ctl_dict['list'].yview)
+        self.document_list_ctl_dict['scrollbar'].pack(side="right", fill="y")
+        self.document_list_ctl_dict['list'].config(yscrollcommand=self.document_list_ctl_dict['scrollbar'].set)
+
+        self.document_list_ctl_dict['button'] = tk.Label(self.document_list_frame, text="Load from files")
+        self.document_list_ctl_dict['button'].pack(fill=tk.BOTH)
+
         self.document_list_ctl_dict['itemKey'] = []
 
 
     def __show_doc_list(self):
         self.document_list_ctl_dict['itemKey'] = []
+        self.document_list_ctl_dict['list'].delete(0, tk.END)
         for idx, record in enumerate(self.docManager.getAllDocs()):
             docId, title, type = record
             if type is "topic":
@@ -163,63 +172,80 @@ class DocManagerGUI(tk.Tk, object):
                 print(docId, " is chosen")
                 self.__show_doc_info(docId)
 
+        def ancAdd_hit():
+            pass
+
+        def deceAdd_hit():
+            pass
 
         # type
         self.document_info_ctl_dict['typeLabel'] = tk.Label(self.document_info_frame, text="type:")
         self.document_info_ctl_dict['typeEntry'] = tk.Entry(self.document_info_frame, show=None)
 
         self.document_info_ctl_dict['typeLabel'].grid(row=0, column=0)
-        self.document_info_ctl_dict['typeEntry'].grid(row=0, column=1, sticky='we')
+        self.document_info_ctl_dict['typeEntry'].grid(row=0, column=1, sticky='w')
 
         # title
         self.document_info_ctl_dict['titleLabel'] = tk.Label(self.document_info_frame, text="title:")
         self.document_info_ctl_dict['titleEntry'] = tk.Entry(self.document_info_frame, show=None)
 
         self.document_info_ctl_dict['titleLabel'].grid(row=1, column=0)
-        self.document_info_ctl_dict['titleEntry'].grid(row=1, column=1, sticky='we')
+        self.document_info_ctl_dict['titleEntry'].grid(row=1, column=1, sticky='w')
 
         # docId
         self.document_info_ctl_dict['docIdLabel'] = tk.Label(self.document_info_frame, text="id:")
         self.document_info_ctl_dict['docIdEntry'] = tk.Entry(self.document_info_frame, show=None)
 
         self.document_info_ctl_dict['docIdLabel'].grid(row=2, column=0)
-        self.document_info_ctl_dict['docIdEntry'].grid(row=2, column=1, sticky='we')
+        self.document_info_ctl_dict['docIdEntry'].grid(row=2, column=1, sticky='w')
 
         # description
         self.document_info_ctl_dict['descLabel'] = tk.Label(self.document_info_frame, text="description:")
-        self.document_info_ctl_dict['descText'] = tk.Text(self.document_info_frame)
+        self.document_info_ctl_dict['descText'] = tkst.ScrolledText(self.document_info_frame, height=4, wrap = tk.WORD)
 
         self.document_info_ctl_dict['descLabel'].grid(row=3, column=0)
-        self.document_info_ctl_dict['descText'].grid(row=3, column=1, sticky='we')
+        self.document_info_ctl_dict['descText'].grid(row=3, column=1, sticky='w')
 
         # ancester
         self.document_info_ctl_dict['ancLabel'] = tk.Label(self.document_info_frame, text="ancesters:")
         self.document_info_ctl_dict['ancList'] = tk.Listbox(self.document_info_frame)
         self.document_info_ctl_dict['deceLabel'] = tk.Label(self.document_info_frame, text="followers:")
         self.document_info_ctl_dict['deceList'] = tk.Listbox(self.document_info_frame)
+        self.document_info_ctl_dict['ancAddButton'] = tk.Button(self.document_info_frame, text="add", command=ancAdd_hit)
+        self.document_info_ctl_dict['deceAddButton'] = tk.Button(self.document_info_frame, text="add", command=deceAdd_hit)
 
         self.document_info_ctl_dict['ancLabel'].grid(row=4, column=0, sticky='we')
         self.document_info_ctl_dict['deceLabel'].grid(row=4, column=1, sticky='we')
         self.document_info_ctl_dict['ancList'].grid(row=5, column=0, sticky='we')
         self.document_info_ctl_dict['deceList'].grid(row=5, column=1, sticky='we')
+        self.document_info_ctl_dict['ancAddButton'].grid(row=6, column=0, sticky='we')
+        self.document_info_ctl_dict['deceAddButton'].grid(row=6, column=1, sticky='we')
+
+        self.document_info_ctl_dict['ancList'].bind("<<ListboxSelect>>", __ancList_select)
+        self.document_info_ctl_dict['deceList'].bind("<<ListboxSelect>>", __deceList_select)
+
 
         # connection description
         self.document_info_ctl_dict['connLabel'] = tk.Label(self.document_info_frame, text="Connection Description:")
-        self.document_info_ctl_dict['connText'] = tk.Text(self.document_info_frame)
+        self.document_info_ctl_dict['connText'] = tkst.ScrolledText(self.document_info_frame, height=4, wrap = tk.WORD)
 
-        self.document_info_ctl_dict['connLabel'].grid(row=6, column=0)
-        self.document_info_ctl_dict['connText'].grid(row=6, column=1, sticky='we')
+        self.document_info_ctl_dict['connLabel'].grid(row=7, column=0)
+        self.document_info_ctl_dict['connText'].grid(row=7, column=1, sticky='w')
+
+        # save info
+        self.document_info_ctl_dict['updateButton'] = tk.Button(self.document_info_frame, text="Update Doc Info", command=ancAdd_hit)
+        self.document_info_ctl_dict['updateButton'].grid(row=8, column=0, sticky='w')
 
 
 
     """ control the sub-module layout """
     def __layout_frames(self):
         self.document_list_frame.config(width=self.windowsParam['GUI_WIDTH']*0.5)
-        self.document_list_frame.pack(side=tk.LEFT, fill=tk.BOTH)
+        self.document_list_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
         self.document_list_frame.pack_propagate(0)
 
         self.document_info_frame.config(width=self.windowsParam['GUI_WIDTH']*0.5)
-        self.document_info_frame.pack(side=tk.LEFT, fill=tk.BOTH)
+        self.document_info_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
         self.document_info_frame.pack_propagate(0)
 
 
@@ -283,12 +309,32 @@ class DocManagerGUI(tk.Tk, object):
 
 
     def __build_GUI(self):
+        def configure(event):
+            if event.width > 200 and event.height > 300:
+                self.windowsParam['GUI_WIDTH'] = event.width
+                self.windowsParam['GUI_HEIGHT'] = event.height
 
+
+        def on_closing():
+            self.__refresh_presetInfo()
+            self.destroy()
+
+
+        self.title("Document Manager %s" % VERSION)
         self.geometry("%dx%d+%d+%d" % (
                     self.windowsParam['GUI_WIDTH'],
                     self.windowsParam['GUI_HEIGHT'],
                     self.winfo_screenwidth()/2 - self.windowsParam['GUI_WIDTH']//2,
                     self.winfo_screenheight()/2 - self.windowsParam['GUI_HEIGHT'] // 2))
+
+        # bind resize event
+        self.bind("<Configure>", configure)
+        # bind close event
+        self.protocol("WM_DELETE_WINDOW", on_closing)
+
+
+
+
 
         # initialize each frame
         self.__init_document_list_frame()
